5b9d69da4a1bc472278e129fef90922595ae8926
Kübra <kubra32karabulut@gmail.com>
2025-02-10 11:31:47 +0300

    Add files via upload

---
+++ odev1/odev1.py
@@ -0,0 +1,4 @@
+ders=['k','o','d','l','a','m','a']
+print(ders[0])
+print(ders[2])
+print(ders[5])
---
+++ odev1/odev2.py
@@ -0,0 +1,4 @@
+ders=['k','o','d','l','a','m','a']
+print(ders[-1])
+print(ders[-4])
+print(ders[-3])
