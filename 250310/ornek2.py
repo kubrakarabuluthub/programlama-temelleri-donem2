da674342e5ce396fc8bcaaad0fcea6ac55a14607
Kübra <kubra32karabulut@gmail.com>
2025-03-10 12:08:24 +0300

    Add files via upload

---
+++ 250310/ornek2.py
@@ -0,0 +1,6 @@
+k_ad=input("kullanıcı adını girin")
+sifre=input("şifre girin")
+if (k_ad=="türkiye" and sifre=="1923"):
+    print("girin başarılı")
+else:
+    print("giriş başarısız")
---
+++ 250310/ornek3.py
@@ -0,0 +1,5 @@
+sayi=int(input("bir sayi girin: "))
+if(sayi %3== and sayi % 5== 0):
+    print("15e bölünür")
+else:
+    print("15e bölünemez")
