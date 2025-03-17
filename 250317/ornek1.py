5fb8df36d5d889c7d1a432f68bbf1c70ff7d2377
Kübra <kubra32karabulut@gmail.com>
2025-03-17 12:06:21 +0300

    Add files via upload

---
+++ 250317/ornek1.py
@@ -0,0 +1,6 @@
+sayi1=int(input("bir sayi girin"))
+sayi2=int(input("bir sayi girin"))
+if sayi1<sayi2:
+    print("sayi1 büyüktür")
+else:
+    print("sayi1 küçüktür")
---
+++ 250317/ornek2.py
@@ -0,0 +1,10 @@
+adet=int(input("kaç tane alacaksın"))
+birim_fiyati=int(input("ürünün birim fiyatını girin"))
+
+toplma=adet * birim fiyatı
+if toplam >=3000:
+    toplam=toplam * 0.80 
+    print("ürünlerin tutarı :",toplam)
+else:
+toplam=toplam-100
+print("ürünlerin tutarı :,toplam")
